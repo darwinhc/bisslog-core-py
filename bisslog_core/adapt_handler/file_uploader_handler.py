ade20715ab7a6a3c17a2cebb8afde0fd8d0f1c6b
Darwin Herrera C <darwinsherrerac@gmail.com>
2025-03-21 15:59:36 -0500

    fix: Delete unnecessary classes

--- bisslog_core/adapt_handler/file_uploader_handler.py
+++ bisslog_core/adapt_handler/file_uploader_handler.py
@@ -5,35 +5,7 @@ from bisslog_core.adapters.base_adapter import BaseAdapter
 from bisslog_core.ports.upload_file import IUploadFile
 
 
-
-class UploadFileBlankBullet(IUploadFile, BaseAdapter):
-
-    def __init__(self, division_name: str):
-        self.division_name = division_name
-
-    @staticmethod
-    def __generate_string(remote_path: str) -> str:
-        separator = "#"*80
-        return "\n" + separator + f"\nUpload file blank bullet with remote path '{remote_path}'\n" + separator
-
-    def upload_file_stream(self, remote_path: str, stream: bytes, *args,
-                           transaction_id: Optional[str] = None, **kwargs) -> bool:
-        string = self.__generate_string(remote_path)
-        self.log.info(string, checkpoint_id="upload-file-stream", transaction_id=transaction_id)
-        return True
-
-
-    def upload_file_from_local(self, local_path: str, remote_path: str, *args,
-                               transaction_id: Optional[str] = None,  **kwargs) -> bool:
-        string = self.__generate_string(remote_path)
-        self.log.info(string, checkpoint_id="upload-file-stream", transaction_id=transaction_id)
-        return True
-
-
 class UploadFileHandler(AdaptHandler):
+    pass
 
-    def generate_blank_adapter(self, item_name: str):
-        return UploadFileBlankBullet(item_name)
-
-
-bisslog_upload_file = UploadFileHandler("upload-file-bisslog")
+bisslog_upload_file = UploadFileHandler("file-uploader")
--- bisslog_core/adapt_handler/publisher_handler.py
+++ bisslog_core/adapt_handler/publisher_handler.py
@@ -4,20 +4,8 @@ from bisslog_core.adapters.base_adapter import BaseAdapter
 from bisslog_core.ports.publisher import IPublisher
 
 
-
-class PublisherBlank(IPublisher, BaseAdapter):
-
-    def __call__(self, queue_name: str, body: object, partition: str = None, *args, **kwargs) -> None:
-        separators = "#" * 80
-        string = (f"\n{separators}\nPublishing on {queue_name} with partition" +
-                  f" {partition}:\nbody: {body}\n{separators}")
-        self.log.info(string, checkpoint_id="bisslog-blank-publisher")
-
-
 class PublisherHandler(AdaptHandler):
-
-    def generate_blank_adapter(self, item_name: str):
-        return PublisherBlank()
+    pass
 
 
-bisslog_pubsub = PublisherHandler("publisher-handler-default")
+bisslog_pubsub = PublisherHandler("publisher-default")
